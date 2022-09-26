d292f7fab6388816c65b82665d7becd4211cd042
MikeWight <michel.wight@hotmail.com>
2022-09-25 21:29:36 -0300

    imperative writting example

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function MainContent() {
-	return <h1>I'm Learning React</h1>;
-}
+// function MainContent() {
+// 	return <h1>I'm Learning React</h1>;
+// }
 
-ReactDOM.render(
-	<div>
-		<MainContent />
-	</div>,
-	document.getElementById("root")
-);
+// ReactDOM.render(
+// 	<div>
+// 		<MainContent />
+// 	</div>,
+// 	document.getElementById("root")
+// );
+
+var newh1 = document.createElement("h1");
+newh1.className = "header";
+newh1.textContent = "This is a imperative way to program";
+document.getElementById("root").append(newh1);
